a839d0b49db32ca3732a8f908f46cd9c0922de9f
2goddie7 <diego.mullo@epn.edu.ec>
2025-11-25 17:59:26 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import "package:flutter/material.dart"; //importa todos los widgest de material design
+import "screens/home_screen.dart"; //importa nuestra pantalla principal
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //Quitar el banner de "DEBUG" en la esquina superior derecha
+      debugShowCheckedModeBanner: false,
+      //Título de la aplicación
+      title: "Mi portafolio",
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+
+      //pantalla inicial
+      home: const HomeScreen(),
+    );
+  }
+}

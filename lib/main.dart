713f3a25638d8d3dbcba8480718ba89436eb3e4d
2goddie7 <diego.mullo@epn.edu.ec>
2025-11-25 17:59:43 -0500

    portafolio movil

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //Quitar el banner de "DEBUG" en la esquina superior derecha
+      //Quitar el banner de "DEBUG" en la esquina superior derecha ajsdgjha
       debugShowCheckedModeBanner: false,
       //Título de la aplicación
       title: "Mi portafolio",
